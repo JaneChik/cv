8ee3c5c51ba7b910a54bc8c7e78ba6c245ff0eed
unknown <jane-poum987@mail.ru>
2022-03-03 18:04:01 +0300

    add cv files

---
+++ setup.R
@@ -0,0 +1,9 @@
+# remotes::install_github('mitchelloharawild/icons@v0.1.0')
+# devtools::install_github("nstrayer/datadrivencv")
+
+datadrivencv::use_datadriven_cv(
+  full_name = "Evgeniia Chikina",
+  data_location = "https://docs.google.com/spreadsheets/d/1flNhCgpHqQze6IjGkt9S8eMzBJz6zA9NqgLFDmE_ug4/edit?usp=sharing",
+  pdf_location = "D:/CV",
+  html_location = "D:/CV"
+)
